2f76baa24cf90e11354e0b92e132f14e10b3e7e6
sjrhee <sjrhee@users.noreply.github.com>
2025-12-14 07:47:06 +0000

    Include crdp.properties and update POM for local JARs

--- scripts/02_prepare_data.sh
+++ scripts/02_prepare_data.sh
@@ -35,7 +35,7 @@ cat > "$SAMPLE_DATA_DIR/02_load_data.sql" <<EOF
 USE mysql_employees;
 SET GLOBAL local_infile=1;
 LOAD DATA INFILE '/docker-entrypoint-initdb.d/employee.csv' INTO TABLE employee FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/salary.csv' INTO TABLE salary FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
+
 EOF
 
 echo "Data preparation complete. SQL scripts created in $SAMPLE_DATA_DIR."
--- scripts/sample_data/02_load_data.sql
+++ scripts/sample_data/02_load_data.sql
@@ -1,4 +1,4 @@
 USE mysql_employees;
 SET GLOBAL local_infile=1;
 LOAD DATA INFILE '/docker-entrypoint-initdb.d/employee.csv' INTO TABLE employee FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/salary.csv' INTO TABLE salary FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
+
